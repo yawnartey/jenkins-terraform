12380475cd7f3fb0147ee4fd27cca0fe2f262f99
Yaw Nartey <yaw.nartey@qubole.com>
2023-09-18 02:06:46 +0200

    fix: updated bucket name

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 #create the S3 bucket for your state files 
 resource "aws_s3_bucket" "jenkins-state-files" {
-  bucket = "jenkins-state-files_1"
+  bucket = "jenkins-state-files-one"
   tags = {
-    Name  = "jenkins-state-files_1"
+    Name  = "jenkins-state-files-one"
   }
 }
 
